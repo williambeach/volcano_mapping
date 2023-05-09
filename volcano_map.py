cf42a8d9b5999c7ab6f1131b55ddfa4e3c19fd17
williambeach <intratech.cs@icloud.com>
2023-05-09 15:28:19 -0400

    add function that changes color of marker according to elevation of volcano

--- volcano_map.py
+++ volcano_map.py
@@ -7,6 +7,31 @@ longitudes = list(volcanoes["Longitude"])
 elevation = list(volcanoes["Elevation (m)"])
 names = list(volcanoes["Volcano Name"])
 types = list(volcanoes["Type"])
+
+def color_by_elev(elv):
+    if elv < 0:
+        return 'darkblue'
+    elif elv >= 0 and elv <= 500:
+        return 'blue'
+    elif elv > 500 and elv <= 1000:
+        return 'lightblue'
+    elif elv > 1000 and elv <= 1500:
+        return 'lightgreen'
+    elif elv > 1500 and elv <= 2000:
+        return 'green'
+    elif elv > 2000 and elv <= 3000:
+        return 'darkgreen'
+    elif elv > 3000 and elv <= 4000:
+        return 'lightred'
+    elif elv > 4000 and elv <= 5000:
+        return 'red'
+    elif elv > 5000 and elv <= 6000:
+        return 'darkred'
+    elif elv > 6000 and elv <= 7000:
+        return 'orange'
+    
+
+
 map = folium.Map(location=[39.094632662122386, -96.4134528100795], zoom_start=4, 
                  tiles="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}&hl=en", attr='Google', max_zoom=100, prefer_canvas=True)
 
@@ -19,7 +44,7 @@ for lat, long, elv, name, type in zip(latitudes, longitudes, elevation, names, t
                   html=f'<p style="text-align: center;"><a href="https://www.google.com/search?q={name}+volcano" target="_blank"><b>{name}</b></a></p>'
                        f'<p style="text-align: center;">{"Elevation: " + str(elv) + " m"}</p>'
                        f'<p style="text-align: center;">{"Type: " + type}</p>'), 
-                  icon=folium.Icon(color='green')))
+                  icon=folium.Icon(color=color_by_elev(elv))))
 
 
 
